048766401fbfe6b8b5056f89d813ea581c4d52d7
remiie <virusloki@ya.ru>
2023-05-15 23:15:52 +0300

    create door struct

---
+++ KeyVision/Modules/Door/Models/Door.swift
@@ -0,0 +1,17 @@
+//
+//  Door.swift
+//  KeyVision
+//
+//  Created by Роман Васильев on 15.05.2023.
+//
+
+import Foundation
+
+struct Door {
+    let name: String?
+    let snapshot: String?
+    let room: String?
+    let id: Int
+    let favorites: Bool
+    var camera: Bool { (snapshot != nil) }
+}
