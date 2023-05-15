69f7582988e2bba4cff4642e4a86c9aaa96792c1
remiie <virusloki@ya.ru>
2023-05-15 16:19:09 +0300

    create protocol for doors view

---
+++ KeyVision/Modules/Door/Protocols/DoorsViewProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  DoorsViewProtocol.swift
+//  KeyVision
+//
+//  Created by Роман Васильев on 15.05.2023.
+//
+
+import UIKit
+
+protocol DoorsViewProtocol: AnyObject {
+    var delegate: DoorsViewDelegate? { get set }
+}
+
+protocol DoorsViewDelegate: AnyObject {
+    func getItemsCount() -> Int
+    func getItem(at index: IndexPath) -> Door
+    func doorHasCamera(at index: IndexPath) -> Bool
+}
