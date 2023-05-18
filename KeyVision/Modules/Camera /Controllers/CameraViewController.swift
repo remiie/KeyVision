88545b6456a89526cef83be6a12d5c0124b964ba
remiie <virusloki@ya.ru>
2023-05-18 09:33:49 +0300

    testing: success Response in camera controller

--- KeyVision/Modules/Camera /Controllers/CameraViewController.swift
+++ KeyVision/Modules/Camera /Controllers/CameraViewController.swift
@@ -16,10 +16,27 @@ class CameraViewController: UIViewController {
         return view
     }()
     
+    private var rooms = [String]()
+    private var cameras = [Camera]()
+    private let apiManager = NetworkManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configure()
+        
+        apiManager.fetchCameras { [self] result in
+            switch result {
+            case .success(let successResponse):
+                guard successResponse.success,
+                      let loadedCameras = successResponse.data.cameras,
+                      let loadedRooms = successResponse.data.room else { return }
+                rooms.append(contentsOf: loadedRooms)
+                cameras.append(contentsOf: loadedCameras)
+            case .failure(let failureResponse):
+                print(failureResponse.localizedDescription) // alert controller
+            }
+        }
     }
     
     
@@ -27,7 +44,7 @@ class CameraViewController: UIViewController {
         view.addSubview(camerasView)
         view.backgroundColor = .systemGray6
         
-        NSLayoutConstraint.activate([
+        NSLayoutConstraint.activate([ // save constant
             camerasView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.size.width/4 + 10),
             camerasView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             camerasView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
